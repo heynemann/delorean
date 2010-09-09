2cbf7138ce82877555f2c5476365a09c88214f16
Bernardo Heynemann <heynemann@gmail.com>
2010-09-08 22:58:48 -0300

    Make for tests

--- tests/test_db.rb
+++ tests/test_db.rb
@@ -1,10 +1,20 @@
 $LOAD_PATH << '../src'
 require 'db'
 
-describe Db, "start" do
+describe Db do
+
   it "initializes a new values dictionary" do
     db = Db.new
     db.values == {}
   end
+
 end
 
+describe Message do
+  
+  it "should create a new message" do
+    msg = Message.new
+    msg
+  end
+  
+end
