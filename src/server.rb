1cf3e418647978a05220e5141922bacb5528399d
Bernardo Heynemann <heynemann@gmail.com>
2010-09-09 12:04:51 -0300

    About and new layout

--- src/server.rb
+++ src/server.rb
@@ -4,7 +4,12 @@ require 'sinatra/base'
 
 class Server < Sinatra::Base
   set :sessions, true
+  set :public, File.dirname(__FILE__) + '/public'
   set :db, Db.new
+
+  get '/about' do
+    haml :about
+  end
 
   get '/catalogues' do
     @catalogues = settings.db.catalogues
